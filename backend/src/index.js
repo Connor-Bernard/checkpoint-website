93e7bcdfcbc6077e31a739af92e31053f399f38f
Connor Bernard <connorbernard@berkeley.edu>
2024-11-12 21:39:26 -0800

    feat: add security middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 
 import { apiRouter } from './api/index.js';
 import { PORT } from './config/env.js';
 
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express().use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            // eslint-disable-next-line quotes
+            defaultSrc: ["'self'"],
+        },
+    }),
+    cors(),
+    rateLimit({
+        windowMs: 15 * 60 * 1000,
+        limit: 100,
+        message: 'Too many requests from this IP, please try again in an hour!',
+    }),
+    express.json(),
+    express.urlencoded({ extended: true }),
+);
 
 apiRouter(app);
 
